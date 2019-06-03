37fb92d362c58a9bfb0db1d6ea396d97b6366958
German Uritskiy <guritsk1@jhu.edu>
2019-06-03 15:38:44 -0400

    v1.2.1

--- bin/metawrap-scripts/split_salmon_out_into_bins.py
+++ bin/metawrap-scripts/split_salmon_out_into_bins.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2.7
 import sys, os
+import numpy as np
 
 # load bins and their contigs
 bins={}
@@ -33,25 +34,34 @@ for salmon_file in os.listdir(sys.argv[1]):
 		ct+=1
 		bin=bins[line.split("\t")[0]]
 		abun=float(line.strip().split("\t")[1])
-	
 		if bin not in bin_abundances:
 			bin_abundances[bin]={}
 			bin_abundances[bin]["total_len"]=0
 			bin_abundances[bin]["total_cov"]=0
+			bin_abundances[bin]["cov_list"]=[]
 			bin_abundances[bin]["samples"]={}
-	
+
+
+		length = contig_lengths[line.split("\t")[0]]
+		weight = length/1000
+
+		for i in range(weight):
+			bin_abundances[bin]["cov_list"].append(abun)
 		bin_abundances[bin]["total_len"]+=contig_lengths[line.split("\t")[0]]
 		bin_abundances[bin]["total_cov"]+=abun * contig_lengths[line.split("\t")[0]]
 	
 	if ct==0:
-		print "None of the contigs/scaffolds in the -a metagenomic assembly file were present in the bin files. Please make sure that the bins and total assembly have the exact same bins. One cause for this could be that you reassembled the bins, disrupring the contig naming. If you do not have the original total metagenomic assembly file, then you could not provide the -a option at all (but this is not ideal for abundance estimation)."
+		sys.stderr.write("\nNone of the contigs/scaffolds in the -a metagenomic assembly file were present in the bin files. Please make sure that the bins and total assembly have the exact same bins. One cause for this could be that you reassembled the bins, disrupring the contig naming. If you do not have the original total metagenomic assembly file, then you could not provide the -a option at all (but this is not ideal for abundance estimation).\n")
 		sys.exit(1)
 		quit()
 
 	for bin in bin_abundances:
-		bin_abundances[bin]["samples"][sample]=bin_abundances[bin]["total_cov"] / bin_abundances[bin]["total_len"]	
+		#bin_abundances[bin]["samples"][sample]=bin_abundances[bin]["total_cov"] / bin_abundances[bin]["total_len"]	
+		bin_abundances[bin]["samples"][sample] = np.median(bin_abundances[bin]["cov_list"])
+		#bin_abundances[bin]["samples"][sample] = sum(bin_abundances[bin]["cov_list"])
                 bin_abundances[bin]["total_len"]=0
                 bin_abundances[bin]["total_cov"]=0
+		bin_abundances[bin]["cov_list"]=[]
 
 
 first=True
